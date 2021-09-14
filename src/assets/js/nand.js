b4a9993b986388e21b7c7f857107d7c38654e95d
RikudoS <rikudo.seni@o2.pl>
2021-09-14 05:51:56 +0200

    bs Legenda

--- src/assets/js/nand.js
+++ src/assets/js/nand.js
@@ -1,4 +1,3 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function toggleNav() {
     navSize = document.getElementById("mySidebar").offsetWidth;
     if (navSize > 0) {
@@ -13,9 +12,10 @@ function openNav() {
     document.getElementById("openbtn").style.marginRight = "250px";
   }
   
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("bord").style.marginRight = "0";
     document.getElementById("openbtn").style.marginRight = "0";
   }
+
+
